38a62952dd6a7704f1c378de34a4267254352b15
Chris <chrisem816@gmail.com>
2019-04-30 14:13:32 -0700

    init init

---
+++ clicky-game/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+import Card from "./components/Card";
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
+function App() {
+  return (
+    <>
+    <Card />
+    </>
+  );
+}
+
+export default App;
---
+++ clicky-game/src/components/Card.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Card(props){
+    return (
+        <div className="jumbotron">
+        <p>Hi</p>
+        </div>
+    );
+}
+
+export default Card;
